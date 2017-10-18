ffbaa858458da7eae28c1797fa19b749a50415eb
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-10-18 18:30:25 +0300

    create blank maven project

---
+++ src/main/java/com/github/netcracker2017team/App.java
@@ -0,0 +1,10 @@
+package com.github.netcracker2017team;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
