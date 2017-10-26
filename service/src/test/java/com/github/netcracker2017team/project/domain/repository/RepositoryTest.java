ded5477fb1aebe0937260ee64a8b96b205fdf4cf
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-10-26 19:41:01 +0300

    create annotation to test repositories

---
+++ service/src/test/java/com/github/netcracker2017team/project/domain/repository/RepositoryTest.java
@@ -0,0 +1,25 @@
+package com.github.netcracker2017team.project.domain.repository;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Alex Ivchenko
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@DataJpaTest(showSql = false)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
+public @interface RepositoryTest {
+
+}
--- service/src/test/java/com/github/netcracker2017team/project/domain/repository/UserRepositoryTest.java
+++ service/src/test/java/com/github/netcracker2017team/project/domain/repository/UserRepositoryTest.java
@@ -1,58 +1,33 @@
 package com.github.netcracker2017team.project.domain.repository;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.netcracker2017team.project.domain.model.User;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Alex Ivchenko
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@DataJpaTest
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
-//@ContextConfiguration(classes = Config.class)
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class,
-        DbUnitTestExecutionListener.class })
+@RunWith(SpringRunner.class)
+@RepositoryTest
 public class UserRepositoryTest {
-//    @Autowired
-//    private TestEntityManager testEntityManager;
-
     @Autowired
     private UserRepository userRepository;
 
     @Test
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @DatabaseSetup("UserRepositoryTest.givenUser_whenFindByUsername_thenReturnUser.given.xml")
     @ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT, value = "UserRepositoryTest.givenUser_whenFindByUsername_thenReturnUser.then.xml")
-    public void name() throws Exception {
-        userRepository.findByUsername("Alex");
+    public void givenUserInDb_whenFind_whenReturnedRightUser() throws Exception {
+        User user = userRepository.findByUsername("Alex");
+        assertThat(user.getUsername()).isEqualTo("Alex");
     }
-
-//    @Test
-//    public void givenUser_whenFindByUsername_thenReturnUser() throws Exception {
-//        User user = User.builder()
-//                .username("test")
-//                .email("test")
-//                .password("pass")
-//                .build();
-//
-//        testEntityManager.persist(user);
-//        testEntityManager.flush();
-//
-//        User found = userRepository.findByUsername(user.getUsername());
-//        assertThat(found).isEqualToComparingFieldByField(user);
-//    }
 }
