217b7ad4672b4e4ad9e0bec25f8443a03c8e7308
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-11-03 22:19:40 +0300

    try to fix deploying error (remove test profile)

--- service/src/test/java/com/github/netcracker2017team/project/domain/repository/RepositoryTest.java
+++ service/src/test/java/com/github/netcracker2017team/project/domain/repository/RepositoryTest.java
@@ -3,7 +3,6 @@ package com.github.netcracker2017team.project.domain.repository;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -18,7 +17,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @DataJpaTest(showSql = false)
-@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
         DirtiesContextTestExecutionListener.class,
