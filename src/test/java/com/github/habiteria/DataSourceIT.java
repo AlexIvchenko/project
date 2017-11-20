9c10f127387f606a793c520919b0e75aa77e9e42
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-11-20 21:52:17 +0300

    customize profiles set ddl-auto to update when work with real db

--- src/test/java/com/github/habiteria/DataSourceIT.java
+++ src/test/java/com/github/habiteria/DataSourceIT.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles({"db", "integration-test-database-defaults"})
+@ActiveProfiles({"real-db", "integration-test-database-defaults"})
 public class DataSourceIT {
     @Test
     public void contextLoads() {
